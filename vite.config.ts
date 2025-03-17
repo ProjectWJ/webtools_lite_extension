e6bc352e9c81524b1c4f1b71648d1fc506266800
ProjectWJ <tlrhdxpa10@gmail.com>
2025-03-17 16:58:52 +0900

    프로젝트 이름 변경 및 TypeScript, vite 추가 및 설정

---
+++ vite.config.ts
@@ -0,0 +1,39 @@
+import { defineConfig } from "vite";
+import { viteStaticCopy } from "vite-plugin-static-copy";
+
+export default defineConfig({
+  build: {
+    outDir: "dist", // 최종 결과물이 들어가는 폴더
+    emptyOutDir: true, // 빌드할 때 dist 비워주기
+    rollupOptions: {
+      input: {
+        popup: "src/popup/popup.html",
+        background: "src/background.ts",
+      },
+      output: {
+        entryFileNames: "src/[name].js", // 파일명 유지하기
+      },
+    },
+    minify: "terser",
+    terserOptions: {
+      compress: {
+        drop_console: true, // console.log 지우기
+        drop_debugger: true,
+      },
+    },
+  },
+  plugins: [
+    viteStaticCopy({
+      targets: [
+        {
+          src: "public/*",
+          dest: ".", // dist에 그대로 복사
+        },
+        {
+          src: "manifest.json",
+          dest: ".", // dist에 복사
+        },
+      ],
+    }),
+  ],
+});
