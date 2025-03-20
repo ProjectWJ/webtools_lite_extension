91f8491ef49a99babb2d24c040469a8a616a0a02
ProjectWJ <tlrhdxpa10@gmail.com>
2025-03-20 17:10:11 +0900

    popup 페이지에서 글자 수 세기 기능 추가, 판넬 토글 구현, 스타일시트 적용

--- src/popup/popup.ts
+++ src/popup/popup.ts
@@ -1,3 +1,54 @@
+/** 웹툴 버튼(글자 수 세기, 대소문자 변환, 단위 변환...) */
+document.getElementById("characterCount")?.addEventListener("click", () => {
+  panelToggle("characterCountPanel");
+});
+document
+  .getElementById("capitalSmallConvert")
+  ?.addEventListener("click", () => {
+    panelToggle("capitalSmallConvertPanel");
+  });
+document.getElementById("unitConvert")?.addEventListener("click", () => {
+  panelToggle("unitConvertPanel");
+});
+document.getElementById("unmeralConvert")?.addEventListener("click", () => {
+  panelToggle("unmeralConvertPanel");
+});
+document.getElementById("korEngConvert")?.addEventListener("click", () => {
+  panelToggle("korEngConvertPanel");
+});
+document
+  .getElementById("exchangeRateCalculate")
+  ?.addEventListener("click", () => {
+    panelToggle("exchangeRateCalculatePanel");
+  });
+document.getElementById("dateCalculate")?.addEventListener("click", () => {
+  panelToggle("dateCalculatePanel");
+});
+document.getElementById("addressConvert")?.addEventListener("click", () => {
+  panelToggle("addressConvertPanel");
+});
+
+/** 돌아가기 버튼 */
+document.querySelectorAll(".backBtn").forEach((button) => {
+  button.addEventListener("click", () => {
+    panelToggle("tabPanel");
+  });
+});
+
+/** 버튼 이벤트 토글 */
+const panels = document.querySelectorAll("[id$=Panel]"); // Panel로 끝나는 단어 검색 $
+
+function panelToggle(id: string) {
+  panels.forEach((panel) => {
+    if (panel.id === id) {
+      panel.classList.remove("blind");
+    } else {
+      panel.classList.add("blind");
+    }
+  });
+}
+
+/** 옵션 */
 const optionsBtn = document.getElementById("optionsBtn");
 
 if (optionsBtn) {
@@ -9,4 +60,62 @@ if (optionsBtn) {
       window.open(chrome.runtime.getURL("src/options/options.html"));
     }
   });
+}
+
+/** 글자 수 세기 */
+document
+  .getElementById("characterCountTextarea")
+  ?.addEventListener("input", characterCountAction);
+function characterCountAction() {
+  const textArea: HTMLTextAreaElement = document.getElementById(
+    "characterCountTextarea"
+  ) as HTMLTextAreaElement;
+  const blankInclude: HTMLElement | null =
+    document.getElementById("blankInclude");
+  const blankIgnore: HTMLElement | null =
+    document.getElementById("blankIgnore");
+
+  if (textArea && blankInclude && blankIgnore) {
+    const textValue: string = textArea.value;
+    const textWithoutSpaces: string = textValue.replace(/\s+/g, "");
+
+    blankInclude.innerText = `공백 포함 ${textValue.length} 자 | ${byteCounter(
+      textValue,
+      1
+    )} Byte`;
+    blankIgnore.innerText = `공백 미포함 ${
+      textWithoutSpaces.length
+    } 자 | ${byteCounter(textWithoutSpaces, 0)} Byte`;
+  }
+}
+
+// 바이트 계산기
+// 출처 https://blog.naver.com/gytks4/222275082864
+/* 
+  function byteCounter(text: string): number {
+  return new TextEncoder().encode(text).length;
+} 유니코드 기준 UTF-8 인코딩 바이트 수를 구해줌
+ 옵션 선택형 UI로 구현할 수도 있음. 클래스 토글 구현 방법 채팅 참조
+*/
+function byteCounter(text: string, blank: number = 0) {
+  let byte = 0;
+
+  if (blank === 0) {
+    // 공백이 미포함이면 미리 줄바꿈과 공백을 빈칸으로 처리
+    text = text.replace(/\s+/g, "");
+  }
+
+  for (let i = 0; i < text.length; i++) {
+    // 한글표현 정규식 : ㄱ-ㅎㅏ-ㅣ가-힣
+    // 한자표현 정규식 : 一-龥
+    // 일본어표현 정규식 : ぁ-ゔァ-ヴー々〆〤
+    // 이 모든것을 /[]/ 안에 포함시켜서 연달아 써주면 "or" 처리됨
+    // 한, 중, 일 언어라면, byte를 2 더해주고, 아니라면 1을 더해주고, 최종적으로 byte를 return
+    if (/[ㄱ-ㅎㅏ-ㅣ가-힣一-龥ぁ-ゔァ-ヴー々〆〤]/.test(text[i])) {
+      byte = byte + 2;
+    } else {
+      byte++;
+    }
+  }
+  return byte;
 }
