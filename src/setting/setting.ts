f64135b386ef09c726db077d4f020bb2f312884b
ProjectWJ <tlrhdxpa10@gmail.com>
2025-07-05 23:25:53 +0900

    설정 페이지 틀 작성, 업데이트 문서 분리, 개인정보처리방침에 이메일 추가

--- src/setting/setting.ts
+++ src/setting/setting.ts
@@ -1,2 +1,38 @@
 import "../import-bulma";
-console.log("옵션 페이지");
+
+// 새로고침 시 페이지 대응
+window.addEventListener("DOMContentLoaded", () => {
+  const url = window.location.hash;
+  const hash = url.substring(2, url.length);
+  if (url !== "") {
+    panelSelectAction(`nav-${hash}`);
+  }
+});
+
+document.getElementById("nav-general")?.addEventListener("click", () => {
+  panelSelectAction("nav-general");
+});
+document.getElementById("nav-information")?.addEventListener("click", () => {
+  panelSelectAction("nav-information");
+});
+
+const navList = document.querySelectorAll(".nav_item");
+const innerPages = document.querySelectorAll("[id$=-panel]");
+
+function panelSelectAction(id: string) {
+  // focus 변경
+  navList.forEach((nav) => {
+    if (nav.id !== id && nav.classList.contains("focus"))
+      nav.classList.remove("focus");
+    if (nav.id === id) nav.classList.add("focus");
+  });
+
+  // inner UI 변경
+  innerPages.forEach((page) => {
+    if (page.id === id.substring(4, id.length) + "-panel") {
+      page.classList.remove("blind");
+    } else {
+      page.classList.add("blind");
+    }
+  });
+}
